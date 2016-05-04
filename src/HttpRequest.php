32d68b53e3f7a59645475a0cf86ed034bbd0d67f
root <root@overbrn.com>
2016-05-04 03:56:04 -0400

    added support for get params

--- src/HttpRequest.php
+++ src/HttpRequest.php
@@ -8,6 +8,11 @@ class HttpRequest {
 	public static function get($url, $params = []) {
 		$ch = curl_init();
 
+		if(count($params) > 0) {
+			$url .= "?".http_build_query($params);
+		}
+		echo $url;
+
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
