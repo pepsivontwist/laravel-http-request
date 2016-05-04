1983453971db934faf172c4c2ff2b458a8ef856d
root <root@overbrn.com>
2016-05-04 04:35:01 -0400

    added support for post requests

--- src/HttpRequest.php
+++ src/HttpRequest.php
@@ -11,10 +11,27 @@ class HttpRequest {
 		if(count($params) > 0) {
 			$url .= "?".http_build_query($params);
 		}
-		echo $url;
 
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+		$output = curl_exec($ch);
+
+		curl_close($ch);
+
+		return $output;
+	}
+
+	public static function post($url, $params = []) {
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_POST, 1);
+
+		if(count($params) > 0) {			
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+		}
+
+		curl_setopt($ch, CURLOPT_URL, $url);
 
 		$output = curl_exec($ch);
 
